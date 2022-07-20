20cf3583c2cb89be800bddb4fc9d8bfefa2cd723
jsankit99 <jsankit99@gmail.com>
2022-07-20 11:42:29 +0545

    deploye ready

--- app.js
+++ app.js
@@ -1,68 +1,62 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var socketIO = require('socket.io')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var socketIO = require("socket.io");
 var app = express();
+var compression = require("compression");
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(compression());
 
+app.get("/", (req, res) => {
+  res.sendFile("login.html");
+});
 
-
-
-app.get('/', (req, res) => {
-  res.sendFile('login.html')
-})
-
-let players = []
-app.post('/login', (req, res) => {
-  console.log(players.length)
+let players = [];
+app.post("/login", (req, res) => {
   if (!players.length) {
     players[0] = [req.body.name || "Guest"];
 
     res.status(200).json({
       id: 0,
-      room: 0
-    })
-  }
-  else if (players[players.length - 1]?.length < 2) {
-    players[players.length - 1].push(req.body.name || "Guest")
+      room: 0,
+    });
+  } else if (players[players.length - 1]?.length < 2) {
+    players[players.length - 1].push(req.body.name || "Guest");
     res.status(200).json({
       id: 1,
-      room: players.length - 1
-    })
-  }
-  else {
-    players[players.length] = [req.body.name]
+      room: players.length - 1,
+    });
+  } else {
+    players[players.length] = [req.body.name];
     res.status(200).json({
       id: 0,
-      room: players.length
-    })
+      room: players.length,
+    });
   }
-
-})
-const server = require('http').createServer(app);
+});
+const server = require("http").createServer(app);
 const io = socketIO(server);
-io.on('connection', socket => {
-  socket.on('join', player => {
-    io.emit('players', players[player.room]?.length)
-  })
-  socket.on('pos', pos => {
-    socket.broadcast.emit('posIncoming', pos)
-  })
-  socket.on('ballPos', ballPos => {
-    socket.broadcast.emit('ballPos', ballPos)
-  })
-  socket.on('start', m => {
-    socket.broadcast.emit('start', true)
-  })
-})
+io.on("connection", (socket) => {
+  socket.on("join", (player) => {
+    io.emit("players", players[player.room]?.length);
+  });
+  socket.on("pos", (pos) => {
+    socket.broadcast.emit("posIncoming", pos);
+  });
+  socket.on("ballPos", (ballPos) => {
+    socket.broadcast.emit("ballPos", ballPos);
+  });
+  socket.on("start", (m) => {
+    socket.broadcast.emit("start", true);
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -73,11 +67,10 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500).json(err);
-
 });
 
-server.listen(8000, () => console.log('Connected to Server'))
+server.listen(8000, "localhost", () => console.log("Connected to Server"));
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -4,270 +4,248 @@ let socket = io();
 let ctx = canvas.getContext("2d");
 let go = "down";
 let go2 = "right";
-
+let instruction = "";
 window.onload = () => {
-    if (!localStorage.getItem('room')) {
-        location.href = '/'
-    }
-    socket.emit('join', {
-        id: localStorage.getItem('id'),
-        room: localStorage.getItem('room')
-    })
-}
+  if (!localStorage.getItem("room")) {
+    location.href = "/";
+  }
+  socket.emit("join", {
+    id: localStorage.getItem("id"),
+    room: localStorage.getItem("room"),
+  });
+  if (localStorage.getItem("id") === 1) {
+    instruction = "Please Move The bat by pressing 'a' and 'd'";
+  } else {
+    instruction = "Please Move The bat by pressing 'Left' and 'Right' arrow";
+  }
+  document.getElementById("instruction").textContent = instruction;
+};
 
 let players = 0;
 
 class Bat {
-    constructor(xPos, yPos, width, height) {
-        this.width = width;
-        this.position = {
-            x: xPos,
-            y: yPos,
-        };
-        this.height = height;
-        this.draw();
-    }
-
-    setPosition(x, y) {
-        this.position = {
-            x,
-            y,
-        };
-    }
-
-    draw = () => {
-        ctx.rect(this.position.x, this.position.y, this.width, this.height);
-        ctx.fillRect(
-            this.position.x,
-            this.position.y,
-            this.width,
-            this.height
-        );
-        ctx.stroke();
+  constructor(xPos, yPos, width, height) {
+    this.width = width;
+    this.position = {
+      x: xPos,
+      y: yPos,
     };
+    this.height = height;
+    this.draw();
+  }
+
+  setPosition(x, y) {
+    this.position = {
+      x,
+      y,
+    };
+  }
+
+  draw = () => {
+    ctx.rect(this.position.x, this.position.y, this.width, this.height);
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    ctx.stroke();
+  };
 }
 class Ball {
-    constructor(xPos, yPos, width, height) {
-        this.position = {
-            x: xPos,
-            y: yPos,
-        };
-
-        this.draw();
-    }
-
-    setPosition(x, y) {
-        this.position = {
-            x,
-            y,
-        };
-    }
-
-
-    move() {
-        if (this.position.x > canvas.width - 10) {
-            go2 = "left";
-        }
-        if (this.position.x < 10) {
-            go2 = "right";
-        }
-
-        if (go2 === "left") {
-            this.position.x -= 2;
-        }
-        if (go2 === "right") {
-            this.position.x += 2;
-        }
-        if (this.position.y > canvas.height - 30) {
-            go = "up";
-        }
-        if (this.position.y < 25) {
-            go = "down";
-        }
-        if (go === "up") {
-            this.position.y -= 3;
-        } else this.position.y += 3;
-    }
-
-    draw = (position1, position2, width) => {
-        if (position1 && position2) {
-            if (this.position.y < 25) {
-                if (
-                    this.position.x < position1.x - 4 ||
-                    this.position.x > position1.x + width + 4
-                ) {
-                    return 0;
-                }
-            }
-            if (this.position.y > canvas.height - 30) {
-                if (
-                    this.position.x < position2.x - 4 ||
-                    this.position.x > position2.x + width + 4
-                ) {
-                    return 0;
-                }
-            }
-        }
-        ctx.beginPath();
-        ctx.arc(this.position.x, this.position.y, 5, 0, 360, false);
-        ctx.stroke();
-        if (localStorage.getItem('id') === '0') {
-            this.move();
-        }
-        else {
-            socket.on('ballPos', ({ ball }) => {
-                this.setPosition(ball.x, ball.y)
-            })
-            // this.move()
-        }
-        return 1;
+  constructor(xPos, yPos, width, height) {
+    this.position = {
+      x: xPos,
+      y: yPos,
     };
+
+    this.draw();
+  }
+
+  setPosition(x, y) {
+    this.position = {
+      x,
+      y,
+    };
+  }
+
+  move() {
+    if (this.position.x > canvas.width - 10) {
+      go2 = "left";
+    }
+    if (this.position.x < 10) {
+      go2 = "right";
+    }
+
+    if (go2 === "left") {
+      this.position.x -= 2;
+    }
+    if (go2 === "right") {
+      this.position.x += 2;
+    }
+    if (this.position.y > canvas.height - 30) {
+      go = "up";
+    }
+    if (this.position.y < 25) {
+      go = "down";
+    }
+    if (go === "up") {
+      this.position.y -= 3;
+    } else this.position.y += 3;
+  }
+
+  draw = (position1, position2, width) => {
+    if (position1 && position2) {
+      if (this.position.y < 25) {
+        if (
+          this.position.x < position1.x - 4 ||
+          this.position.x > position1.x + width + 4
+        ) {
+          return 0;
+        }
+      }
+      if (this.position.y > canvas.height - 30) {
+        if (
+          this.position.x < position2.x - 4 ||
+          this.position.x > position2.x + width + 4
+        ) {
+          return 0;
+        }
+      }
+    }
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, 5, 0, 360, false);
+    ctx.stroke();
+    if (localStorage.getItem("id") === "0") {
+      this.move();
+    } else {
+      socket.on("ballPos", ({ ball }) => {
+        this.setPosition(ball.x, ball.y);
+      });
+      // this.move()
+    }
+    return 1;
+  };
 }
 class Game {
-    constructor() {
-        this.keyconstraints = {
-            a: {
-                pressed: false,
+  constructor() {
+    this.keyconstraints = {
+      a: {
+        pressed: false,
 
-                do: () => {
-                    this.bat2.setPosition(
-                        this.bat2.position.x - 3,
-                        this.bat2.position.y
-                    );
-                },
-            },
-            d: {
-                pressed: false,
-                do: () => {
-                    this.bat2.setPosition(
-                        this.bat2.position.x + 4,
-                        this.bat2.position.y
-                    );
-                },
-            },
-            ArrowLeft: {
-                pressed: false,
-                do: () => {
-                    this.bat.setPosition(
-                        this.bat.position.x - 3,
-                        this.bat.position.y
-                    );
-                },
-            },
+        do: () => {
+          this.bat2.setPosition(this.bat2.position.x - 3, this.bat2.position.y);
+        },
+      },
+      d: {
+        pressed: false,
+        do: () => {
+          this.bat2.setPosition(this.bat2.position.x + 4, this.bat2.position.y);
+        },
+      },
+      ArrowLeft: {
+        pressed: false,
+        do: () => {
+          this.bat.setPosition(this.bat.position.x - 3, this.bat.position.y);
+        },
+      },
 
-            ArrowRight: {
-                pressed: false,
-                do: () => {
-                    this.bat.setPosition(
-                        this.bat.position.x + 4,
-                        this.bat.position.y
-                    );
-                },
-            },
-        };
-        this.bat = new Bat(10, 10, 50, 10);
-        this.bat2 = new Bat(10, canvas.height - 30, 50, 10);
-        this.ball = new Ball(40, 40);
-        this.moveBat();
+      ArrowRight: {
+        pressed: false,
+        do: () => {
+          this.bat.setPosition(this.bat.position.x + 4, this.bat.position.y);
+        },
+      },
+    };
+    this.bat = new Bat(10, 10, 50, 10);
+    this.bat2 = new Bat(10, canvas.height - 30, 50, 10);
+    this.ball = new Ball(40, 40);
+    this.moveBat();
+  }
 
+  animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    socket.on("posIncoming", ({ bat1, bat2 }) => {
+      if (localStorage.getItem("id") === "1") {
+        this.bat.setPosition(bat1.x, bat1.y);
+      } else {
+        this.bat2.setPosition(bat2.x, bat2.y);
+      }
+    });
+    Object.keys(this.keyconstraints).forEach((key) => {
+      if (this.keyconstraints[key].pressed) {
+        if (key === "a" || key === "d") {
+          localStorage.getItem("id") === "1" && this.keyconstraints[key].do();
+        } else {
+          localStorage.getItem("id") === "0" && this.keyconstraints[key].do();
+        }
+        if (localStorage.getItem("id") === "0") {
+          socket.emit("pos", {
+            bat1: this.bat.position,
+          });
+        } else {
+          socket.emit("pos", {
+            bat2: this.bat2.position,
+          });
+        }
+      }
+    });
+
+    const gameRunning = this.ball.draw(
+      this.bat.position,
+      this.bat2.position,
+      this.bat.width
+    );
+
+    if (!gameRunning) {
+      window.cancelAnimationFrame(this.animate);
+      ctx.font = "30px Arial";
+      ctx.fillText("Game Over", canvas.width / 2 - 80, canvas.height / 2);
+      return;
     }
+    this.bat.draw();
+    this.bat2.draw();
+    if (localStorage.getItem("id") === "0") {
+      socket.emit("ballPos", {
+        ball: this.ball.position,
+      });
+    }
+    window.requestAnimationFrame(this.animate);
+  };
 
-    animate = () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        socket.on('posIncoming', ({ bat1, bat2 }) => {
-            if (localStorage.getItem('id') === '1') {
-                this.bat.setPosition(bat1.x, bat1.y)
-            }
-            else {
-                this.bat2.setPosition(bat2.x, bat2.y)
-            }
-        })
-        Object.keys(this.keyconstraints).forEach((key) => {
-            if (this.keyconstraints[key].pressed) {
-                if (key === 'a' || key === 'd') {
-                    localStorage.getItem('id') === '1' && this.keyconstraints[key].do();
-                }
-                else {
-                    localStorage.getItem('id') === '0' && this.keyconstraints[key].do()
-                }
-                if (localStorage.getItem('id') === '0') {
-                    socket.emit('pos', {
-                        bat1: this.bat.position,
-                    })
-                }
-                else {
-                    socket.emit('pos', {
-                        bat2: this.bat2.position
-                    })
-                }
-
-            }
-        });
-
-        const gameRunning = this.ball.draw(
-            this.bat.position,
-            this.bat2.position,
-            this.bat.width
-        );
-
-        if (!gameRunning) {
-            window.cancelAnimationFrame(this.animate);
-            ctx.font = '30px Arial';
-            ctx.fillText('Game Over', canvas.width / 2 - 80, canvas.height / 2)
-            return;
-        }
-        this.bat.draw();
-        this.bat2.draw();
-        if (localStorage.getItem('id') === '0') {
-            socket.emit('ballPos', {
-                ball: this.ball.position
-            })
-        }
-        window.requestAnimationFrame(this.animate);
-    };
-
-    moveBat = () => {
-        window.addEventListener("keydown", (e) => {
-            if (
-                e.key === "a" ||
-                e.key === "d" ||
-                e.key == "ArrowLeft" ||
-                e.key === "ArrowRight"
-            ) {
-                this.keyconstraints[e.key].pressed = true;
-            }
-        });
-        window.addEventListener("keyup", (e) => {
-            if (
-                e.key === "a" ||
-                e.key === "d" ||
-                e.key == "ArrowLeft" ||
-                e.key === "ArrowRight"
-            ) {
-                this.keyconstraints[e.key].pressed = false;
-            }
-        });
-    };
+  moveBat = () => {
+    window.addEventListener("keydown", (e) => {
+      if (
+        e.key === "a" ||
+        e.key === "d" ||
+        e.key == "ArrowLeft" ||
+        e.key === "ArrowRight"
+      ) {
+        this.keyconstraints[e.key].pressed = true;
+      }
+    });
+    window.addEventListener("keyup", (e) => {
+      if (
+        e.key === "a" ||
+        e.key === "d" ||
+        e.key == "ArrowLeft" ||
+        e.key === "ArrowRight"
+      ) {
+        this.keyconstraints[e.key].pressed = false;
+      }
+    });
+  };
 }
 const game = new Game();
-socket.on('players', player => {
-    if (player === 2) {
-        console.log(player, localStorage.getItem('id'))
-        if (localStorage.getItem('id') === '0') {
-            let start = document.createElement('button');
-            start.textContent = 'Start';
-            document.body.appendChild(start)
-            start.onclick = () => {
-                game.animate();
-                socket.emit('start', true)
-            }
-        }
+socket.on("players", (player) => {
+  if (player === 2) {
+    if (localStorage.getItem("id") === "0") {
+      let start = document.createElement("button");
+      start.textContent = "Start";
+      document.body.appendChild(start);
+      start.onclick = () => {
+        game.animate();
+        socket.emit("start", true);
+      };
     }
-})
+  }
+});
 
-
-
-socket.on('start', msg => {
-    console.log('here')
-    game.animate();
-})
+socket.on("start", (msg) => {
+  game.animate();
+});
